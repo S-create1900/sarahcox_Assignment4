ecbde8d04e42bc98698eab6a2e490f2f40f1aff1
S-create1900 <saphron_m@yahoo.com>
2025-02-15 18:04:17 -0700

    Updated code
    Updated code to remove all code to print to console.

--- src/com/coderscampus/a4/Main.java
+++ src/com/coderscampus/a4/Main.java
@@ -10,28 +10,16 @@ public class Main {
 	public static void main(String[] args) {
         // Create an instance of StudentService to read and process the CSV
         new StudentService();
-
-        // Print the contents of the CSV files to the console
-        printCsvFile("course1.csv");
-        System.out.println();
-        printCsvFile("course2.csv");
-        System.out.println();
-        printCsvFile("course3.csv");
-
     }
 
-    // Method to read and print a CSV file and its contents to the console
+    // Method to read a CSV file
     private static void printCsvFile(String csvFile) {  
-        System.out.println(csvFile);
-        System.out.println();
         try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
             String line;
-            // Read and print each line of the CSV file
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+
             }
         } catch (IOException e) {
-//            System.err.println("Error reading file: " + filePath);
             e.printStackTrace();
         }
     }
